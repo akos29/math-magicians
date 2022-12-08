7b3705a5891c40ce4481dd079e36d94e08d248e7
LAPTOP-4QVU197N\KB <kibrish49@gmail.com>
2022-12-08 18:26:40 +0200

    Add the Calculator functionality foreach buttons

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,62 +1,56 @@
-/* eslint-disable react/no-unused-state */
 /* eslint-disable react/destructuring-assignment */
-/* eslint-disable react/prefer-stateless-function */
-import React from 'react';
+import React, {} from 'react';
+import Calculate from '../logic/calculate';
 
 class Calculator extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { result: 0, operator: '', operand: 0 };
+    this.state = {
+      total: 0,
+      next: null,
+      operation: null,
+    };
   }
 
   handleChange = (e) => {
-    const op = e.target.innerHTML;
-    this.setState({ operand: op });
-  }
-
-  handleOperator =(e) => {
-    this.setState({ operator: e.target.innerHTML });
-    this.reset();
-  }
-
-  reset = () => {
-    this.setState({ result: 0 });
+    const res = Calculate(this.state, e.target.innerHTML);
+    this.setState(res);
   }
 
   render() {
     return (
       <div className="calc">
         <form className="row-group">
-          <input disabled className="result" value={this.state.operand} onChange={(e) => { this.setState({ operand: e.target.value }); }} />
+          <input disabled className="result" value={this.state.next || this.state.total || 0} />
         </form>
         <div className="row-group">
-          <button type="button" id="AC" onClick={() => this.setState({ result: 0 })} className="keys">AC</button>
-          <button type="button" onClick={(e) => this.handleOperator(e)} id="+/-" className="keys">+/-</button>
-          <button type="button" onClick={(e) => this.handleOperator(e)} className="keys">%</button>
-          <button type="button" onClick={(e) => this.handleOperator(e)} className="keys">&#247;</button>
+          <button type="button" id="AC" onClick={this.handleChange} className="keys">AC</button>
+          <button type="button" onClick={this.handleChange} className="keys">+/-</button>
+          <button type="button" onClick={this.handleChange} className="keys">%</button>
+          <button type="button" onClick={this.handleChange} className="keys">&#247;</button>
         </div>
         <div className="row-group">
-          <button className="keys" type="button" onClick={(e) => this.handleChange(e)}>7</button>
-          <button className="keys" type="button" onClick={(e) => this.handleChange(e)}>8</button>
-          <button className="keys" type="button" onClick={(e) => this.handleChange(e)}>9</button>
-          <button className="keys" type="button" onClick={(e) => this.handleOperator(e)}>X</button>
+          <button className="keys" type="button" onClick={this.handleChange}>7</button>
+          <button className="keys" type="button" onClick={this.handleChange}>8</button>
+          <button className="keys" type="button" onClick={this.handleChange}>9</button>
+          <button className="keys" type="button" onClick={this.handleChange}>x</button>
         </div>
         <div className="row-group">
-          <button type="button" onClick={(e) => this.handleChange(e)} className="keys">4</button>
-          <button type="button" onClick={(e) => this.handleChange(e)} className="keys">5</button>
-          <button type="button" onClick={(e) => this.handleChange(e)} className="keys">6</button>
-          <button type="button" onClick={(e) => this.handleOperator(e)} className="keys">-</button>
+          <button type="button" onClick={this.handleChange} className="keys">4</button>
+          <button type="button" onClick={this.handleChange} className="keys">5</button>
+          <button type="button" onClick={this.handleChange} className="keys">6</button>
+          <button type="button" onClick={this.handleChange} className="keys">-</button>
         </div>
         <div className="row-group">
-          <button type="button" onClick={(e) => this.handleChange(e)} className="keys">1</button>
-          <button type="button" onClick={(e) => this.handleChange(e)} className="keys">2</button>
-          <button type="button" onClick={(e) => this.handleChange(e)} className="keys">3</button>
-          <button type="button" onClick={(e) => this.handleOperator(e)} className="keys">+</button>
+          <button type="button" onClick={this.handleChange} className="keys">1</button>
+          <button type="button" onClick={this.handleChange} className="keys">2</button>
+          <button type="button" onClick={this.handleChange} className="keys">3</button>
+          <button type="button" onClick={this.handleChange} className="keys">+</button>
         </div>
         <div className="row-group">
-          <button type="button" onClick={(e) => this.handleChange(e)} className="keys zero">0</button>
-          <button type="button" onClick={(e) => this.handleChange(e)} className="keys">.</button>
-          <button type="submit" onClick={(e) => this.handleOperator(e)} className="keys">=</button>
+          <button type="button" onClick={this.handleChange} className="keys zero">0</button>
+          <button type="button" onClick={this.handleChange} className="keys">.</button>
+          <button type="button" onClick={this.handleChange} className="keys">=</button>
         </div>
       </div>
     );
