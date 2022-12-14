7e17516c3d2190f4ceffc857f31ecff2ff43bda7
LAPTOP-4QVU197N\KB <kibrish49@gmail.com>
2022-12-14 23:09:03 +0200

    Add home, about and calculator js and use react router

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from './Navbar';
+
+function Header() {
+  return (
+    <div className="header">
+      <h1>Math Magicians</h1>
+      <Navbar />
+    </div>
+  );
+}
+
+export default Header;
